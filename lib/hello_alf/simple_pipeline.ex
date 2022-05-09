4a7473f7f502518612cafe03071fd0586db2d309
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2022-05-09 14:08:37 +0200

    Add dummy docs to SimplePipeline

--- lib/hello_alf/simple_pipeline.ex
+++ lib/hello_alf/simple_pipeline.ex
@@ -1,4 +1,6 @@
 defmodule HelloAlf.SimplePipeline do
+  @moduledoc "It's a very simple pipeline"
+
   use ALF.DSL
 
   @components [
@@ -7,13 +9,16 @@ defmodule HelloAlf.SimplePipeline do
     stage(:mult_two, count: 10)
   ]
 
+  @doc "it does nothing"
   def do_nothing(event, _), do: event
 
+  @doc "it adds one and sleeps a bit"
   def add_one(event, _) do
     Process.sleep(1)
     event + 1
   end
 
+  @doc "it multiplies by two"
   def mult_two(event, _) do
     Process.sleep(2)
     event * 2
